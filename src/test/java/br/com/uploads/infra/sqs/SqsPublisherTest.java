87b63dcbc7d0a7892de654a04198ec0bbfd2b0bc
Filipe de Deus <filipedeus2015@gmail.com>
2025-07-01 21:27:55 -0300

    fixing sqs publisher unit tests

--- src/test/java/br/com/uploads/infra/sqs/SqsPublisherTest.java
+++ src/test/java/br/com/uploads/infra/sqs/SqsPublisherTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import reactor.core.publisher.Mono;
-import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
 
 import static br.com.uploads.webui.constants.Constants.EMAIL_CONTEXT_KEY;
+import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -22,7 +23,7 @@ import static org.mockito.MockitoAnnotations.openMocks;
 class SqsPublisherTest {
 
   @Mock
-  private SqsClient sqsClient;
+  private SqsAsyncClient sqsClient;
 
   @InjectMocks
   private SqsPublisher sqsPublisher;
@@ -42,7 +43,8 @@ class SqsPublisherTest {
   @Test
   void sendMessage_shouldSendMessageAndReturnResponse() {
     SendMessageResponse response = SendMessageResponse.builder().messageId("123").build();
-    when(sqsClient.sendMessage(any(SendMessageRequest.class))).thenReturn(response);
+    when(sqsClient.sendMessage(any(SendMessageRequest.class)))
+      .thenReturn(completedFuture(response));
 
     var context = reactor.util.context.Context.of(EMAIL_CONTEXT_KEY, "test@email.com");
     Mono<SendMessageResponse> result = sqsPublisher.sendMessage("body").contextWrite(context);
